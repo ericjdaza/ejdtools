cf2c6353750ffaa81971d061eb609869fc34abf5
Eric Jay Daza <ericjaydaza@Erics-MacBook-Pro.local>
2021-01-10 01:32:46 -0800

    updated fancyTable.R

--- fancyTable.R
+++ fancyTable.R
@@ -11,13 +11,13 @@ fancyTable <- function(
     cbind(
       n,
       (n %>% prop.table * 100) %>% round(round_digits) %>% paste0("%")
-    )
+    ) %>% knitr::kable("simple")
   )
   if (length(varnames) == 2) return(
     cbind(
       n,
       (n %>% prop.table(margin = margin_value) * 100) %>% round(round_digits) %>% paste0("%") %>% matrix(ncol=ncol(n))
-    )
+    ) %>% knitr::kable("simple")
   )
   
 }

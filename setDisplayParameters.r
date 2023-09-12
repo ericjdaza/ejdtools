23c8555c7b333c0756185fea4cdb91b42cdf3e61
Eric Jay Daza <edaza@evidation.com>
2023-09-11 22:05:16 -0700

    updated setDisplayParameters.r

--- setDisplayParameters.r
+++ setDisplayParameters.r
@@ -1,44 +1,25 @@
 # Function to set gtsummary display parameters.
 setDisplayParameters <- function(
     add_n_obs = FALSE, # set to TRUE to append total number of observations
-    percent = FALSE # set to TRUE to append "%"
+    percent = FALSE, # set to TRUE to append "%" for continuous variables that represent percentages
+    percent_categorical = TRUE, # set to TRUE to append "%" to "{p}" for categorical variables
+    footnote_non_mece = "\\>1 response allowed per participant for the following variables (i.e., percentages may sum to >100%): "
 ) {
     
-    ifelse(
-        percent == FALSE,
-        {
-            # statistic_all_continuous <- c(
-            #     "{N_nonmiss} ({p_nonmiss})",
-            #     "{mean} ({sd})",
-            #     "{median} ({p25}, {p75})",
-            #     "{min}, {max}",
-            #     "{N_miss} ({p_miss})"
-            # ),
-            statistic_all_continuous <- c(
-                "{mean} ({sd})",
-                "{median} ({p25}, {p75})",
-                "{min}, {max}",
-                "{N_miss} ({p_miss})"
-            )            
-            statistic_all_categorical <- c("{n} ({p})")
-        },
-        {
-            # statistic_all_continuous <- c(
-            #     "{N_nonmiss} ({p_nonmiss})",
-            #     "{mean} ({sd})",
-            #     "{median} ({p25}, {p75})",
-            #     "{min}, {max}",
-            #     "{N_miss} ({p_miss})"
-            # )
-            statistic_all_continuous <- c(
-                "{mean}% ({sd}%)",
-                "{median}% ({p25}%, {p75}%)",
-                "{min}%, {max}%",
-                "{N_miss} ({p_miss})"
-            )
-            statistic_all_categorical <- c("{n} ({p}%)")
-        }
+    statistic_all_continuous <- c(
+        "{mean} ({sd})",
+        "{median} ({p25}, {p75})",
+        "{min}, {max}",
+        "{N_miss} ({p_miss})"
+    )            
+    if (percent == TRUE) statistic_all_continuous <- c(
+        "{mean}% ({sd}%)",
+        "{median}% ({p25}%, {p75}%)",
+        "{min}%, {max}%",
+        "{N_miss} ({p_miss})"
     )
+    statistic_all_categorical <- c("{n} ({p})")
+    if (percent_categorical == TRUE) statistic_all_categorical <- c("{n} ({p}%)")
     if (add_n_obs == TRUE) {
         
         statistic_all_continuous <- c(
@@ -48,18 +29,7 @@ setDisplayParameters <- function(
         statistic_all_categorical <- c("{n} ({p}%) (N={N})")
         
     }
-    # statistic_list <- list(
-    #     all_continuous() ~ statistic_all_continuous,
-    #     all_categorical() ~ statistic_all_categorical
-    # )
     
-    # digits_all_continuous <- c(
-    #     c(0, 1),
-    #     c(1, 2),
-    #     c(1, 0, 0),
-    #     c(0, 0),
-    #     c(0, 1)
-    # )
     digits_all_continuous <- c(
         c(1, 2),
         c(1, 0, 0),
@@ -76,18 +46,7 @@ setDisplayParameters <- function(
         digits_all_categorical <- c(0, 1, 0)
         
     }
-    # digits_list <- list(
-    #     all_continuous() ~ digits_all_continuous,
-    #     all_categorical() ~ digits_all_categorical
-    # )
-
-    # label_all_continuous <- c(
-    #     "n (%)",
-    #     "Mean (SD)",
-    #     "Median (IQR)",
-    #     "Min, Max",
-    #     "Missing (n (%))"
-    # )
+    
     label_all_continuous <- c(
         "Mean (SD)",
         "Median (IQR)",
@@ -101,27 +60,21 @@ setDisplayParameters <- function(
             "N",
             label_all_continuous
         )
-        # label_all_categorical <- c("n (%) (N)")
         
     }
-    # label_list <- list(
-    #     all_continuous() ~ label_all_continuous,
-    #     all_categorical() ~ label_all_categorical
-    # )
     
     list(
         
         statistic_all_continuous = statistic_all_continuous,
         statistic_all_categorical = statistic_all_categorical,
-        # statistic_list,
         
         digits_all_continuous = digits_all_continuous,
         digits_all_categorical = digits_all_categorical,
-        # digits_list = digits_list,
         
         label_all_continuous = label_all_continuous,
-        label_all_categorical = label_all_categorical
-        # , label_list = label_list
+        label_all_categorical = label_all_categorical,
+        
+        footnote_non_mece = footnote_non_mece
         
     )
     
